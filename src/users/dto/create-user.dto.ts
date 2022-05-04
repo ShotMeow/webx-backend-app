047ac7f3dd282ac28340f53d14ce0913c38436d6
ShotMeow <shotmeow@gmail.com>
2022-05-04 22:29:12 +0300

    Add WebX API Docs on Swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    example: 'Иванов Иван Иванович',
+    description: 'ФИО пользователя',
+  })
   readonly fullName: string;
+  @ApiProperty({
+    example: 'username@gmail.com',
+    description: 'E-mail пользователя',
+  })
   readonly email: string;
+  @ApiProperty({ example: 'qwerty1234', description: 'Пароль пользователя' })
   readonly password: string;
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,5 @@
 import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
 
 interface UserCreationAttrs {
   fullName: string;
@@ -8,6 +9,7 @@ interface UserCreationAttrs {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttrs> {
+  @ApiProperty({ example: '1', description: 'Уникальный идентификатор' })
   @Column({
     type: DataType.INTEGER,
     unique: true,
@@ -16,15 +18,25 @@ export class User extends Model<User, UserCreationAttrs> {
   })
   id: number;
 
+  @ApiProperty({
+    example: 'Иванов Иван Иванович',
+    description: 'ФИО пользователя',
+  })
   @Column({ type: DataType.STRING, allowNull: false })
   fullName: string;
 
+  @ApiProperty({
+    example: 'username@gmail.com',
+    description: 'E-mail пользователя',
+  })
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
 
+  @ApiProperty({ example: 'qwerty1234', description: 'Пароль пользователя' })
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
 
+  @ApiProperty({ example: 'true', description: 'Статус аккаунта пользователя' })
   @Column({ type: DataType.BOOLEAN, defaultValue: false })
   banned: boolean;
 }
